6b978d912180a30373e2045719a14dea98cb118a
BernarditaSeguel <ber.seguel@duocuc.cl>
2022-07-06 18:35:28 -0400

    1

---
+++ js/pop-up/pop-up-news-letter.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    function showpopupnewsletter(){
+        $(`.pop-up-news-letter`).addClass(`show`);
+        $(`.pop-up-news-letter-wrap`).addClass(`show`);
+    }
+
+    $(`.pop-up-news-letter-suscripcion-img`).click(function(){
+        $(`.pop-up-news-letter`).removeClass(`show`);
+        $(`.pop-up-news-letter-wrap`).removeClass(`show`);
+    });
+    setTimeout(showpopupnewsletter, 5000);
+});
